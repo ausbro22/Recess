7b7756ea9d1cbe8962c232211f85fa41b5887fc7
Justice Koonce <jkooncec.@gmail.com>
2017-10-19 22:08:45 -0400

    Initial component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { StartComponent } from './components/start/start.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    StartComponent
   ],
   imports: [
     BrowserModule
