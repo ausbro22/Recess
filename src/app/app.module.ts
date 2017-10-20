68bff5589fcc8d49491a61a2a510116866d4415f
Justice Koonce <jkooncec.@gmail.com>
2017-10-20 15:57:38 -0400

    GUI Frame start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,35 @@
+// modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
+//services
+// import { DataService } from './services/data.service';
+
+// Componets
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { StartComponent } from './components/start/start.component';
 import { QueueComponent } from './components/queue/queue.component';
 import { FrontComponent } from './components/front/front.component';
-import { PlaygroundComponent } from './components/playground/playground.component';
+import { PlaygroundComponent } from './components/playground/playground.component'
+import { GameComponent } from './components/game/game.component';
+import { PlayerComponent } from './components/player/player.component';
+;
+
+const appRoutes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'login', component:LoginComponent},
+  {path: 'start', component:StartComponent},
+  {path: 'front', component:FrontComponent},
+  {path: 'playground', component:PlaygroundComponent},
+  {path: 'game', component:GameComponent},
+  {path: 'player', component:PlayerComponent},
+];
 
 @NgModule({
   declarations: [
@@ -17,7 +39,9 @@ import { PlaygroundComponent } from './components/playground/playground.componen
     StartComponent,
     QueueComponent,
     FrontComponent,
-    PlaygroundComponent
+    PlaygroundComponent,
+    GameComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule
