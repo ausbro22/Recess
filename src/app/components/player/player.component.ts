96faa812f7276518dd36da894bc80a08aa00e9ab
Justice Koonce <jkooncec.@gmail.com>
2017-11-21 16:01:58 -0500

    More work

--- src/app/components/player/player.component.ts
+++ src/app/components/player/player.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PlayerDataService } from '../../services/player-data.service';
 
 @Component({
   selector: 'app-player',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./player.component.css']
 })
 export class PlayerComponent implements OnInit {
+  message:String;
+  player:Players[];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private playerService:PlayerDataService) {
+    console.log("constructor ran");
   }
 
+  ngOnInit() {
+    this.message = "Let's get started: ";
+
+    // this.playerService.addPlayerToDB();
+  }
+
+  AddPlayer() {
+  
+    this.message = "Player Entered";
+  }
+
+}
+
+interface Players{
+  Name:string,
+  Club:string,
+  Country:string
 }
